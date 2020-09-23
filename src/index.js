7d4326a3a3ac269d46f237696a3d349f386cf414
“ErikBorge” <erikaasborge@gmail.com>
2020-09-23 22:32:40 +0200

    getWeather and api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {render} from 'react-dom';
 
 import Test from './components/Test';
@@ -17,12 +17,33 @@ import './styles/index.scss';
 // }
 //
 // document.body.appendChild(component());
+const api = {
+  key: "209d013b886466ecf743dce3799e8925",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 
 const App = () => {
+
+  const [weather, setWeather] = useState({});
+  const query = 'Oslo';
+
+  const getWeather = () => {
+    // if (e.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result),
+          console.log(result)
+        });
+    // }
+
+  }
   return (
     <div>Yellow dis is App at top level yes.
+    <div className="app"></div>
       <Test/>
+      <button className="search" onClick={getWeather}>get weather</button>
     </div>
   );
 }
