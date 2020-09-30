b417b98eab44eb5c9f553a5147c8295d34c82a8e
“ErikBorge” <erikaasborge@gmail.com>
2020-09-30 17:04:00 +0200

    fixed rain, clear and clouds

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,37 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import Frame from './Frame';
-
-import WindIcon from '../assets/wind.svg';
-// import RandomShape from '../assets/randomshape.svg';
-
-// import flatten from 'lodash-es/flatten'
-// import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
-// // potensielt hente paths fra svg (med imports rett over)
-// const svgResource = new Promise(resolve =>
-//   windsvg = new SVGLoader().load(WindIcon, shapes => {
-//     console.log(shapes);
-//     shapes.paths.map(element => {
-//       element.color.b = 255;
-//       element.color.g = 255;
-//       element.color.r = 255;
-//     })
-//
-//   }
-//
-//     // resolve(flatten(shapes.map((group, index) => group.toShapes(true).map(shape => ({ shape, color: group.color, index })))))
-//   )
-// )
+// import WindIcon from '../assets/wind.svg';
 
 const Weather = (props) => {
   const [mainStatement, setMainStatement] = useState('');
   const [sideStatement, setSideStatement] = useState('');
   const [quote, setQuote] = useState('');
 
-
   useEffect(() => {
     generateStatement();
-    changeSVGColor();
     // console.log(sideStatement);
     // const svg = document.getElementById("randomshapepath");
     // console.log(svg);
@@ -39,14 +17,12 @@ const Weather = (props) => {
 
   const {name, icon, main, temp, wind, clouds, rain, time, timezone} = props.weather;
   const hour = new Date((time+timezone) * 1000).getUTCHours();
-  // const index = props.index;
-  const phrases = ['Ikke så verst.', 'Helt OK.', 'Litt kjipt.', 'Ganske kjipt.', 'Helt jævlig.', 'Ikke tenk på å gå ut en gang.'];
+
+  const phrases = ['Ikke verst.', 'Kunne vært bedre.', 'Helt OK.', 'Litt kjipt.', 'Ganske kjipt.', 'Helt jævlig.', 'Ikke gå ut.'];
   const windStatements = [0, 'litt vind', 'noe vind', 'en del vind', 'ganske mye vind', 'helt sinnssykt mye vind'];
   const rainStatements = [0, 'nesten ikke regn', 'vanlig-jakkeregn', 'regnjakkeregn', 'paraplyregn', 'støvleregn' ];
   const goodWeatherQuotes = ['Nå kan det bare gå nedover.', 'Kunne vært vær, si!', 'Været slår nok tilbake snart.', 'Ikke se for lyst på det. Det blir verre.'];
   const badWeatherQuotes = ['Ah mah gaaad!', 'I dag igjen...', 'Æsj ass.', 'Blir aldri bra dette.', 'Når blir det sommer igjen?'];
-  // const [index, setIndex] = useState(Math.round(Math.random() * 5));
-  // const index = Math.round(Math.random() * phrases.length)
 
   const generateStatement = () => {
     const windIndex = Math.min(
@@ -78,10 +54,7 @@ const Weather = (props) => {
 
     return phraseIndex;
   }
-  const changeSVGColor = () =>{
 
-  }
-  // console.log(window.innerWidth);
   return (
     <div className="weather">
       {/*<img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt=""/><br/>*/}
@@ -118,7 +91,7 @@ const Weather = (props) => {
                 <h2>{quote}</h2>
               </div>
               <div className="weather__info-graphics">
-                <Frame weather={main}/>
+                <Frame weather={props.weather}/>
               </div>
               <div className="weather__info-specs">
                 <h2>{temp}°</h2>
---
+++ src/components/bits/Clouds.js
@@ -0,0 +1,52 @@
+import React, { useRef, useMemo, useEffect, useState } from 'react';
+import * as THREE from 'three';
+
+import Cloud1 from '../../assets/cloud.png';
+import Cloud2 from '../../assets/cloud2.png';
+
+const Clouds = ({weather}) => {
+  //max 10 clouds
+  const numClouds = Math.round(weather.clouds/10);
+  const cloudArr = new Array(numClouds).fill();
+
+  if (cloudArr && cloudArr.length > 0) {
+    return (
+      <group>
+        {cloudArr.map((_, index) => (
+          <Cloud key={index} />
+        ))}
+      </group>
+    )
+  }
+  else{
+    return null;
+  }
+}
+
+export default Clouds;
+
+const Cloud = () => {
+  const mesh = useRef(null);
+
+  //load the cloud images into cloudImages
+  const cloud1 = useMemo((res) => new THREE.TextureLoader().load(Cloud1), [Cloud1]);
+  const cloud2 = useMemo((res) => new THREE.TextureLoader().load(Cloud2), [Cloud2]);
+  const cloudImages = [cloud1, cloud2];
+
+  //constants for cloud size and position
+  const cloudWidth = Math.random()*(2.5-1)+1;
+  const cloudSize = [cloudWidth, cloudWidth];
+  const cloudMinX = -1.8+cloudWidth/2;
+  const cloudMaxX = 1.8-cloudWidth/2;
+  const cloudPosition = [Math.random()*(cloudMaxX-cloudMinX)+cloudMinX, Math.random()*(cloudMaxX-cloudMinX)+cloudMinX,1];
+  const cloudRotation = Math.random()*Math.PI*2;
+
+  return (
+    <mesh ref={mesh} position={cloudPosition} rotation={[0,0,cloudRotation]}>
+      <planeBufferGeometry attach="geometry" args={cloudSize}/>
+      <meshLambertMaterial attach="material" transparent>
+        <primitive attach="map" object={cloudImages[Math.round(Math.random())]} />
+      </meshLambertMaterial>
+    </mesh>
+  )
+}
